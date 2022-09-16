421b09b4d5c9b15f136822eaac849af523009625
asebn1 <asebn121@gmail.com>
2022-09-16 11:19:01 +0900

    feat: ServletTest 완료

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,18 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> ReflectionTestUtil.executeMethod(junit4Test, method));
     }
 }
